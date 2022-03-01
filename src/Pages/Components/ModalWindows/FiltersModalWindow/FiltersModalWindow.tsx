c1663b3f520d8d4e5138a2a636bad4a17a8b4f10
Shabdanov Nursultan Bekturganovich <shabdanov_n@auca.kg>
2022-03-01 20:15:24 +0600

    Work on filters modal

--- src/Pages/Components/ModalWindows/FiltersModalWindow/FiltersModalWindow.tsx
+++ src/Pages/Components/ModalWindows/FiltersModalWindow/FiltersModalWindow.tsx
@@ -1,7 +1,48 @@
 import React from 'react';
 import cn from "classnames";
 import './FiltersModalWindow.scss'
-import Button from "../../Button/Button";
+import Select from 'react-select'
+import makeAnimated from 'react-select/animated';
+
+interface IOption{
+    value: string
+    label: string
+}
+
+const genres: IOption[] = [
+    { value: 'Adult', label: 'Adult' },
+    { value: 'Adventure', label: 'Adventure' },
+    { value: 'Biography', label: 'Biography' },
+    { value: 'Business fiction', label: 'Business fiction' },
+    { value: 'Classics', label: 'Classics' },
+    { value: 'Children\'s', label: 'Children\'s' },
+    { value: 'Crime', label: 'Crime' },
+    { value: 'Detective', label: 'Detective' },
+    { value: 'Fantasy', label: 'Fantasy' },
+    { value: 'History', label: 'History' },
+    { value: 'Horror', label: 'Horror' },
+    { value: 'Literary Fiction', label: 'Literary Fiction' },
+    { value: 'Mystery', label: 'Mystery' },
+    { value: 'Poetry', label: 'Poetry' },
+    { value: 'Romans', label: 'Romans' },
+    { value: 'SCI-FI', label: 'SCI-FI' },
+    { value: 'Thriller', label: 'Thriller' }
+]
+
+const cities: IOption[] = [
+    { value: 'Bishkek', label: 'Bishkek' },
+    { value: 'Osh', label: 'Osh' },
+    { value: 'Kara-Kol', label: 'Kara-Kol' },
+    { value: 'Talas', label: 'Talas' },
+    { value: 'Djalal-Abad', label: 'Djalal-Abad' },
+    { value: 'Toktogul', label: 'Toktogul' },
+    { value: 'Naryn', label: 'Naryn' },
+    { value: 'LA', label: 'LA' },
+    { value: 'NYC', label: 'NYC' },
+    { value: 'Las-Vegas', label: 'Las-Vegas' }
+]
+
+const animatedComponents = makeAnimated();
 
 const FiltersModalWindow = () => {
     return (
@@ -9,23 +50,40 @@ const FiltersModalWindow = () => {
             <p className={cn('title')}>Filters</p>
             <div className={cn('filters')}>
                 <div className={cn('genre')}>
-                    {/*<p className={cn('filter-title')}>Genre</p>*/}
-                    <select className={cn('select')} value={'Horror'}
-                            title={'Genre'} />
+                    <p className={cn('filter-title')}>Genre</p>
+                    <Select classNamePrefix={'custom-select'}
+                            options={genres} placeholder={'Choose...'}
+                            isSearchable
+                            isMulti
+                            components={animatedComponents}
+                    />
                 </div>
                 <div className={cn('city')}>
-                    {/*<p className={cn('filter-title')}>Genre</p>*/}
-                    <select className={cn('select')} value={'Bishkek'}
-                            title={'City'} />
-                </div>
-                <div className={cn('price')}>
-                    {/*<p className={cn('filter-title')}>Genre</p>*/}
-                    <select className={cn('select')} value={'1000'}
-                            title={'Price'} />
+                    <p className={cn('filter-title')}>City</p>
+                    <Select classNamePrefix={'custom-select'}
+                            options={cities} placeholder={'Choose...'}
+                            isSearchable
+                    />
                 </div>
                 <div className={cn('checkboxes')}>
-                    <input type={"checkbox"} title={'Bookcrossing'}/>
-                    <input type={"checkbox"} title={'Free'}/>
+                    <div className={cn('checkbox-group')}>
+                        <input type="radio" id="price"
+                               name="filter-radio" value="price"
+                               checked={true}/>
+                        <label htmlFor="price">Price</label>
+                    </div>
+
+                    <div className={cn('checkbox-group')}>
+                        <input type="radio" id="free"
+                               name="filter-radio" value="free" />
+                        <label htmlFor="free">Free</label>
+                    </div>
+
+                    <div className={cn('checkbox-group')}>
+                        <input type="radio" id="bookcrossing"
+                               name="filter-radio" value="bookcrossing" />
+                        <label htmlFor="bookcrossing">Bookcrossing</label>
+                    </div>
                 </div>
             </div>
             <button>Done</button>
